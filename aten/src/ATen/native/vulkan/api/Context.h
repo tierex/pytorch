4486c70814fa8cd08326ff3240b7ec749d673430
Ashkan Aliabadi <ashkanaliabadi@fb.com>
2020-08-19 16:27:38 -0700

    Vulkan descriptor and descriptor layout cache.
    ghstack-source-id: 8e10be39d0ec064921208034a36305c662c082cf
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/42642

--- aten/src/ATen/native/vulkan/api/Context.h
+++ aten/src/ATen/native/vulkan/api/Context.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <ATen/native/vulkan/api/Common.h>
+#include <ATen/native/vulkan/api/Descriptor.h>
 #include <ATen/native/vulkan/api/Pipeline.h>
 #include <ATen/native/vulkan/api/Shader.h>
 
@@ -49,6 +50,10 @@ class Context final {
     return pipeline_;
   }
 
+  inline Descriptor& descriptor() {
+    return descriptor_;
+  }
+
  private:
   // Construction and destruction order matters.  Do not move members around.
   Handle<VkInstance, decltype(&VK_DELETER(Instance))> instance_;
@@ -59,6 +64,7 @@ class Context final {
   VkQueue queue_;
   Shader shader_;
   Pipeline pipeline_;
+  Descriptor descriptor_;
 };
 
 bool available();
