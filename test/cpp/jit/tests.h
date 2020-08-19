739f0b8d7a4721124183a8b1ce88f2aaeaff2941
Mikhail Zolotukhin <mvz@fb.com>
2020-08-19 13:21:58 -0700

    [Do not commit][TensorExpr] Add prim::TypeCheck op.
    ghstack-source-id: a5320918018d543d6a266d0af9892813b0e332a2
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/43172

--- test/cpp/jit/tests.h
+++ test/cpp/jit/tests.h
@@ -109,7 +109,8 @@ namespace jit {
   _(MobileNamedParameters)                        \
   _(MobileSaveLoadData)                           \
   _(LiteSGD)                                      \
-  _(FusionAliasing)
+  _(FusionAliasing)                               \
+  _(TypeCheck)
 
 #if defined(USE_CUDA)
 #define TH_FORALL_TESTS_CUDA(_)   \
